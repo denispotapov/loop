6621be3768d9ab906fa67bdace4d48a5a9ee1a9f
Denis Potapov <fantom.r45@mail.ru>
2019-11-09 16:01:48 +0500

    number output

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Array;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i;
+        for (i = 0; i < 20; i++)
+            System.out.println(i);
+   }
+}
+
